bb74401ea1fc60dd8a235b8f1d1b3638b89ce488
Matboud <m.matboud@gmail.com>
2023-09-30 18:02:43 +0200

    adding service workers and add task flow

--- todo_list/src/app/component/dashboard/dashboard.component.ts
+++ todo_list/src/app/component/dashboard/dashboard.component.ts
@@ -1,10 +1,69 @@
 import { Component } from '@angular/core';
+import { CrudService } from 'src/app/service/crud.service';
+import { Task } from 'src/app/model/task';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.css'],
 })
 export class DashboardComponent {
+  taskObj: Task = new Task();
+  taskArr: Task[] = [];
 
+  addTaskValue: string = '';
+
+  constructor(private crudService: CrudService) {}
+
+  ngOnInit(): void {
+    this.taskObj = new Task();
+    this.taskArr = [];
+    this.getAllTasks();
+  }
+
+  getAllTasks() {
+    this.crudService.getAllTask().subscribe(
+      (res) => {
+        this.taskArr = res;
+      },
+      (err) => {
+        alert('enable to get the tasks');
+      }
+    );
+  }
+
+  addTask() {
+    this.taskObj.task_name = this.addTaskValue;
+    this.crudService.addTask(this.taskObj).subscribe(
+      (res) => {
+        this.ngOnInit();
+        this.addTaskValue = ""
+      },
+      (err) => {
+        alert(err);
+      }
+    );
+  }
+
+  editTask() {
+    this.crudService.editTask(this.taskObj).subscribe(
+      (res) => {
+        this.ngOnInit();
+      },
+      (err) => {
+        alert('failed to update task');
+      }
+    );
+  }
+
+  deleteTask(etask: Task) {
+    this.crudService.deleteTask(etask).subscribe(
+      (res) => {
+        this.ngOnInit();
+      },
+      (err) => {
+        alert('failed to delete task');
+      }
+    );
+  }
 }
