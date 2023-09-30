ccdd878b0f5d820f42f1b7dfe0f3597d2993e6dd
Matboud <m.matboud@gmail.com>
2023-09-30 12:03:11 +0200

    adding models and rendering dashboard component

--- todo_list/src/app/app.module.ts
+++ todo_list/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from "@angular/common/http";
 
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './component/dashboard/dashboard.component';
@@ -10,7 +12,9 @@ import { DashboardComponent } from './component/dashboard/dashboard.component';
     DashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- todo_list/src/app/model/task.ts
+++ todo_list/src/app/model/task.ts
@@ -1,2 +1,6 @@
 export class Task {
+    id: number = 0;
+    task_name: string = "";
+    
+
 }
