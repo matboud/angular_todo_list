bb947848821c3f93ab0e74336274de5ffdd8230a
Matboud <m.matboud@gmail.com>
2023-09-30 12:07:20 +0200

    init db.json with tasks

--- todo_list/src/app/service/crud.service.ts
+++ todo_list/src/app/service/crud.service.ts
@@ -1,9 +1,13 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CrudService {
+  serviceURL: string;
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+    this.serviceURL = 'http://localhost:3000/tasks';
+  }
 }
